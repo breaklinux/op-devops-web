5096511490d93b2ac045a5dccee6b67ad3bbe333
breaklinux <13164278484@163.com>
2021-09-27 18:32:55 +0800

    Update instance.js

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,42 +1,42 @@
 import request from '@/utils/request'
 
 export function getInstanceList(query) {
-  return request({
-    url: 'https://op-apis.mumway.com/op-cicd-api/instance/api/v1',
-    method: 'get',
-    params: query
-  })
+    return request({
+        url: 'https://op-apis.breaklinux.com/op-cicd-api/instance/api/v1',
+        method: 'get',
+        params: query
+    })
 }
 
 export function getAppnameList(query) {
-  return request({
-    url: 'https://op-apis.mumway.com/op-cicd-api/app/api/v1',
-    method: 'get',
-    params: query
-  })
+    return request({
+        url: 'https://op-apis.breaklinux.com/op-cicd-api/app/api/v1',
+        method: 'get',
+        params: query
+    })
 }
 
 
 export function postInstance(data) {
-  return request({
-    url: 'https://op-apis.mumway.com/op-cicd-api/instance/api/v1',
-    method: 'post',
-    data
-  })
+    return request({
+        url: 'https://op-apis.breaklinux.com/op-cicd-api/instance/api/v1',
+        method: 'post',
+        data
+    })
 }
 
 export function deleteInstance(data) {
-  return request({
-    url: `https://op-apis.mumway.com/op-cicd-api/instance/api/v1`,
-    method: 'delete',
-    data
-  })
+    return request({
+        url: `https://op-apis.breaklinux.com/op-cicd-api/instance/api/v1`,
+        method: 'delete',
+        data
+    })
 }
 
 export function putEditInstance(data) {
-  return request({
-    url: `https://op-apis.mumway.com/op-cicd-api/instance/api/v1`,
-    method: 'put',
-    data
-  })
+    return request({
+        url: `https://op-apis.breaklinux.com/op-cicd-api/instance/api/v1`,
+        method: 'put',
+        data
+    })
 }
