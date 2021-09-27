64e332186cb45a62cd1094c276f83bed6b804983
breaklinux <13164278484@163.com>
2021-09-27 18:33:58 +0800

    Update user.js

--- src/api/user.js
+++ src/api/user.js
@@ -1,52 +1,52 @@
 import request from '@/utils/request'
 
 export function login(data) {
-  return request({
-    url: '/vue-element-admin/user/login',
-    method: 'post',
-    data
-  })
+    return request({
+        url: '/vue-element-admin/user/login',
+        method: 'post',
+        data
+    })
 }
 
 export function getInfo(token) {
-  return request({
-    url: '/vue-element-admin/user/info',
-    method: 'get',
-    params: { token }
-  })
+    return request({
+        url: '/vue-element-admin/user/info',
+        method: 'get',
+        params: { token }
+    })
 }
 export function getLdapUserList(query) {
-  return request({
-    url: 'https://op-apis.mumway.com/op-cicd-api/ldap/api/v1',
-    method: 'get',
-    params: query
-  })
+    return request({
+        url: 'https://op-apis.breaklinux.com/op-cicd-api/ldap/api/v1',
+        method: 'get',
+        params: query
+    })
 }
 
 
 export function postLdapUserList(data) {
-  return request({
-    url: 'https://op-apis.mumway.com/op-cicd-api/ldap/api/v1',
-    method: 'post',
-    data
-  })
+    return request({
+        url: 'https://op-apis.breaklinux.com/op-cicd-api/ldap/api/v1',
+        method: 'post',
+        data
+    })
 }
 
 
 export function deletedapUserList(data) {
-  return request({
-    url: `https://op-apis.mumway.com/op-cicd-api/ldap/api/v1`,
-    method: 'delete',
-    data
-  })
+    return request({
+        url: `https://op-apis.breaklinux.com/op-cicd-api/ldap/api/v1`,
+        method: 'delete',
+        data
+    })
 }
 
 
 
 
 export function logout() {
-  return request({
-    url: '/vue-element-admin/user/logout',
-    method: 'post'
-  })
+    return request({
+        url: '/vue-element-admin/user/logout',
+        method: 'post'
+    })
 }
